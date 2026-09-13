fdec3b470f5fe36f58441a639ee8f776c6ccd2f0
miailisman30 <michaeluoutnath@gmail.com>
2026-09-13 17:01:13 +0200

    simple CLI use added - 13-09-2026 17:01

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use clap::Parser;
+use std::io::{BufRead, BufReader};
+// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct CLI {
+    // The pattern to look for
+    pattern: String,
+    // The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = CLI::parse();
+
+    let file = std::fs::File::open(&args.path)
+        .with_context(|| format!("could not open file '{}'", args.path.display()))?;
+    let content = BufReader::new(file);
+
+    for line in content.lines() {
+        let line = line.with_context(|| format!("could not read file '{}'", args.path.display()))?;
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
